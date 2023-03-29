edf8f3f459f7f87c3dce14141e800c04520b32f1
cgit6 <cgit67534@gmail.com>
2023-03-30 01:05:26 +0800

    first commit

---
+++ CH3/3.5.comprehensions.py
@@ -0,0 +1,1 @@
+# List,Dict,Set,Generator of Comprehensions
---
+++ CH3/3.6 coding_lesson.py
@@ -0,0 +1,40 @@
+''' 
+該腳本功能:
+在終端機執行coding_lesson.py <偵測的文檔>
+'''
+from sys import argv
+# argv 會返回 [執行檔案名稱.py 被偵測的文檔名稱.py]
+print(f"{argv}")
+print(f"資料長度{len(argv)}")
+print("<---------------------------------->")
+
+# 如果資料長度小於2
+if len(argv) < 2:
+    print("請提供文件名。")
+else:
+    # 開啟文檔
+    file = open(argv[1])
+    # 文檔內容
+    lines = file.read()
+    # 分割的依據為"\n"
+    lines = lines.split("\n")
+
+    # 計算字元數
+    letter_count = 0
+    # 算字數
+    word_count = 0
+    for line in lines:
+        # 分割的依據為" "
+        words = line.split(" ")
+        word_count += len(words)
+        letter_count += len(line)
+
+    # 計算行數 = 長度 - 1(空格)
+    line_count = len(lines)
+
+    # 輸出
+    print(lines)
+    print(type(lines))
+    print(f"line count is {line_count}")
+    print(f"word count is {word_count}")
+    print(f"letter count is {letter_count}")
