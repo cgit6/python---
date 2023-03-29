71b41b5241e895d9f04f67f7d5453a4eb3801a43
cgit6 <cgit67534@gmail.com>
2023-03-30 03:02:57 +0800

    2023/3/30

--- CH3/3.1if.py
+++ CH3/3.1if.py
@@ -1,7 +1,25 @@
 # if statement 
-if(True):
+
+'''
+condition 裡面一定要返回一個Boolean值
+'''
+if (True):
+    # block of code 
     pass 
-elif(True):
+elif (True):
     pass  
 else:
     pass 
+
+'''
+condition
+Q1:介於兩個值之間 
+=> 8 < x < 65
+
+Q2:and 與 & 的差別?
+A2:  
+'''
+
+if ((5>3) and []):
+    pass
+
--- CH3/3.2 loop.py
+++ CH3/3.2 loop.py
@@ -1,13 +1,72 @@
+'''
+loop 常用於重複的事情
+在iterable object (string,list,dict,tuple,set)皆可使用loop
+
+for variable in iterable:
+    do thing
+'''
 # for loop (str,list,tuple,dict)
-for i in "abc":
+str = "abc"
+for i in str:
+    pass
+
+# list
+list = [1,3,5,7,9]
+for i in list:
     print(i)
 
+# tuple
+tuple = [(1,3),(3,5),(5,7),(7,9)]
+for i in tuple:
+    print(i)
+
+for i,j in tuple:
+    print(i,j)
+
+# dictionary 
+dict = {"key1": "value1", "key2": "value2", "key3": "value3"}
+for key in dict.keys():
+    pass
+for value in dict.values():
+    pass
+for item in dict.items():
+    pass
+
+# set
+set = {1,2,3,4,5,6,7,8,9}
+for i in set:
+    pass
+
+'''
+while loop
+當不確定要執行幾次時使用 while
+
+while (if condition=true):
+    do thing
+
+有可能存在無限迴圈的問題
+'''
 # while loop str
 t = 0
 while t < 5:
     print(t)
-    t += 1
+    t += 1 # 一定要有這個
+
+'''
+outer loop: 外面那層
+inner loop: 裡面那層
+'''
 # nested for loop
+for i in "1234":
+    for j in "abcd":
+        pass
+
 
 # pass , break , continue
+'''
+pass: 跳過
 
+用於 for loop
+break: 中止這個迭代過程
+continue: 中止這次迭代
+'''
