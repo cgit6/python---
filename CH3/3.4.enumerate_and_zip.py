2b99a2169bdc7c9bf44359696d3683184231d0b4
cgit6 <cgit67534@gmail.com>
2023-03-31 01:14:05 +0800

    2023/3/31

--- CH3/3.4.enumerate_and_zip.py
+++ CH3/3.4.enumerate_and_zip.py
@@ -1,1 +1,23 @@
-# enumerate and zip
+'''
+Enumerate function: 枚舉函數把所有的元素遍歷出來 
+'''
+
+counters = 0
+for i in "1234567890":
+    if counters< 3:
+        print(i)
+    counters += 1
+
+# enumerate fun 會return 一個 tuple(index, value)
+for count,iter in enumerate("1234567890"):
+    if count<5:
+        print(iter)
+
+# zip fun  把多個list 合併成一個tuple
+
+list1 = [1,2,3,4,5,6,7,8]
+list2 = ["a","b","c","d","e","f","g"]
+
+for i in zip(list1, list2):
+    # 合成最少數量
+    print(i)
--- CH3/3.5.comprehensions.py
+++ CH3/3.5.comprehensions.py
@@ -1,1 +1,42 @@
-# List,Dict,Set,Generator of Comprehensions
+"""
+comprehension: 一種程式縮寫的方法 ，更pythonic
+syntax: 
+new_list = [operation for variable in original_list if condition]
+new_dict = {key:value(operation) for variable in original_dict if condition}
+
+"""
+# List,Dict,Set,Generator of Comprehension
+
+# 從:
+list = [1,2,3,4,5,6]
+squared_x = []
+for item in list:
+    squared_x.append(item**2)
+
+print(squared_x)
+# 變成:
+new_list = [item**2 for item in list ]
+print(new_list)
+# 加入condition
+new_list = [item**2 for item in list if item > 2 ]
+print(new_list)
+
+# dict comprehension
+dict = {'a':1, 'b':2, 'c':3, 'd':4}
+# dict => new_dict
+new_dict = { item : item **2 for item in dict.values()}
+print(new_dict)
+# list => new_dict
+new_dict2 = {item:item**2 for item in list}
+print(new_dict2)
+
+# set comprehension
+# list => set
+new_set = {item**2 for item in list}
+print(new_set)
+# set => set
+set = {1,2,3,4,5,6}
+new_set2 = {item**2 for item in set }
+print(new_set2)
+
+# generator comprehension
