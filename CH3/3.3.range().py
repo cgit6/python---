90214c18825cec190f8de2ff6660e79d03fa0e1d
cgit6 <cgit67534@gmail.com>
2023-03-31 01:15:19 +0800

    2023/3/31

--- CH3/3.3.range().py
+++ CH3/3.3.range().py
@@ -1,1 +1,15 @@
+'''
+range (起始值(預設為0),終止值,間隔)
+起始值:為包含(可選)
+終止值:為不包含
+間隔:(可選)
+'''
 # Range()
+for i in range(0,100,1):
+    print(i)
+
+# 轉成list
+list = list(range(0,100,1))
+print(list)
+
+# imprvement range
